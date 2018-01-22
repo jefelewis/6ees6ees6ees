e352b8fae1880b4efdca1f7d91a589bbb52cf501
jefelewis <jeffylew77@gmail.com>
2018-01-22 13:27:07 -0800

    Finished Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,15 @@
 class Grub {
+
+  // Constructor
+  constructor(age, color, food){
+    this.age = 0;
+    this.color = "pink";
+    this.food = "jelly"
+  }
+
+  // METHODS
+  eat(){
+    return "Mmmmmmmmm jelly";
+  }
+
 };
